68bff595d2d89e1022a7066985eca85c1f7a5b7e
hyeryoung <kimhyeryoung@gimhyelyeong-ui-MacBookPro.local>
2021-06-18 05:32:21 +0900

    first commit

---
+++ Section01/Board.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section01
+{ 
+    class Board
+    {
+        const char CIRCLE = '\u25cf';
+
+        public TileType[,] Tile { get; private set; } //배열
+        public int Size { get; private set; }
+
+
+        public int DestY { get; private set; }
+        public int DestX { get; private set; }
+
+        Player _player;
+       
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+
+        public void Initialize(int size, Player player)
+        {
+            if (size % 2 == 0)
+                return;
+
+            _player = player;
+
+            Tile = new TileType[size, size];
+            Size = size;
+
+            DestY = Size - 2;
+            DestX = Size - 2;
+
+
+            //Mazes for Programmers
+            //GenerateByBinaryTree();
+            GenerateBySideWinder();
+        }
+
+        void GenerateBySideWinder()
+        {
+            //일단 길을 다 막아버리는 작업
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    //벽
+                    if (x % 2 == 0 || y % 2 == 0)
+                        Tile[y, x] = TileType.Wall;
+                    else
+                        Tile[y, x] = TileType.Empty;
+                }
+
+            }
+
+            //랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            //Binary Tree Algorithm
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                int count = 1;
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == Size - 2 && x == Size - 2) //마지막 점
+                    {
+                        continue; //출구로 인식하지 않도록 막아줌
+                    }
+
+                    if (y == Size - 2) //외곽 - 1 좌표
+                    {
+                        Tile[y, x + 1] = TileType.Empty;
+                        continue; //랜덤함수를 타지 않도록 continue
+                    }
+
+                    if (x == Size - 2) //외곽 - 1 좌표
+                    {
+                        Tile[y + 1, x] = TileType.Empty;
+                        continue; //랜덤함수를 타지 않도록 continue
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        //우측
+                        Tile[y, x + 1] = TileType.Empty;
+                        count++;
+                    }
+                    else
+                    {
+                        int randomIndex = rand.Next(0, count);
+                        //아래
+                        Tile[y + 1, x - randomIndex * 2] = TileType.Empty;
+                        count = 1;
+                    }
+                    
+                }
+
+            }
+        }
+
+        /*
+        void GenerateByBinaryTree()
+        {
+            //일단 길을 다 막아버리는 작업
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    //벽
+                    if (x % 2 == 0 || y % 2 == 0)
+                        Tile[y, x] = TileType.Wall;
+                    else
+                        Tile[y, x] = TileType.Empty;
+                }
+
+            }
+
+            //랜덤으로 우측 혹은 아래로 길을 뚫는 작업
+            //Binary Tree Algorithm
+            Random rand = new Random();
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    if (y == Size - 2 && x == Size - 2) //마지막 점
+                    {
+                        continue; //출구로 인식하지 않도록 막아줌
+                    }
+
+                    if (y == Size - 2) //외곽 - 1 좌표
+                    {
+                        Tile[y, x + 1] = TileType.Empty;
+                        continue; //랜덤함수를 타지 않도록 continue
+                    }
+
+                    if (x == Size - 2) //외곽 - 1 좌표
+                    {
+                        Tile[y + 1, x] = TileType.Empty;
+                        continue; //랜덤함수를 타지 않도록 continue
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                    {
+                        //우측
+                        Tile[y, x + 1] = TileType.Empty;
+                    }
+                    else
+                    {
+                        //아래
+                        Tile[y + 1, x] = TileType.Empty;
+
+                    }
+                }
+
+            }
+        }
+        */
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int y = 0; y < Size; y++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    //플레이어 좌표를 갖고와서, 그 좌표랑 현재 y,x가 일치하면 플레이어 전용 색상으로 표시
+                    if (y == _player.PosY && x == _player.PosX)
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    else if (y == DestY && x == DestX)
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    else
+                        Console.ForegroundColor = GetTileColor(Tile[y, x]);
+
+                    Console.Write(CIRCLE + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //랜더링을 하게돼도 이전 상태에 영향을 주지 않기 위해 복원
+            Console.ForegroundColor = prevColor;
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
+        }
+
+
+    }
+}
---
+++ Section01/ListType.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Section01
+{
+    //동적 배열
+    /*
+    class MyList<T>
+    {
+        const int DEFAULTSize = 1;
+        T[] _data = new T[DEFAULTSize];
+
+        public int Count = 0; //실제로 사용중인 데이터 개수 
+        public int Capacity { get { return _data.Length; } } //예약된 데이터 개수
+
+        // O(1) 예외 케이스 : 이사 비용은 무시한다.
+        public void Add(T item)
+        {
+            //1. 공간이 충분히 남아있는지 확인한다.
+            if (Count >= Capacity)
+            {
+                //공간을 다시 늘려서 확보한다.
+                T[] newArray = new T[Count * 2];
+                for (int i = 0; i < Count; i++)
+                    newArray[i] = _data[i];
+                _data = newArray;
+            }
+
+            //2. 공간에다가 데이터를 넣어준다.
+            _data[Count] = item;
+            Count++;
+        }
+
+        // O(1)
+        public T this[int index]
+        {
+            get { return _data[index]; }
+            set { _data[index] = value; }
+        }
+
+        // O(N)
+        public void RemoveAt(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+                _data[i] = _data[i + 1];
+            _data[Count - 1] = default(T); //Index값 초기화
+            Count--;
+        }
+    }
+    */
+
+    //연결 리스트
+    class MyLinkedListNode<T>
+    {
+        public T Data;
+        public MyLinkedListNode<T> Next; //참조값. 방의 이전/다음 주소
+        public MyLinkedListNode<T> Prev;
+
+    }
+
+    class MyLinkedList<T>
+    {
+        public MyLinkedListNode<T> Head = null; //첫번째 방
+        public MyLinkedListNode<T> Tail = null; //마지막 방
+        public int Count = 0;
+
+        public MyLinkedListNode<T> AddLast(T data)
+        {
+            MyLinkedListNode<T> newRoom = new MyLinkedListNode<T>();
+            newRoom.Data = data;
+
+            //만약에 아직 방이 아예 없엇다면,새로 추가한 첫번째 방이 곧 Head
+            if (Head == null)
+                Head = newRoom;
+            //기존의 Tail과 새로 추가되는 방을 연결
+            if (Tail != null)
+            {
+                Tail.Next = newRoom;
+                newRoom.Prev = Tail;
+            }
+            //새로 추가되는 방을 Tail로 인정
+            Tail = newRoom;
+            Count++;
+            return newRoom;
+        }
+
+        //101 102 103 104 105
+        public void Remove(MyLinkedListNode<T> room)
+        {
+            //기존의 첫번째 방의 다음 방을 첫번째 방으로 인정
+            if (Head == room)
+                Head = Head.Next;
+
+            //기존의 마지막 방의 이전 방을 마지막 방으로 인정
+            if (Tail == room)
+                Tail = Tail.Prev;
+
+            //삭제될 방의 앞 뒤 방을 연결
+            if (room.Prev != null)
+                room.Prev.Next = room.Next;
+            if (room.Next != null)
+                room.Next.Prev = room.Prev;
+
+            Count--;
+        }
+
+    }
+
+
+
+    //배열, 동적 배열, 연결 리스트 비교
+    class ListType
+    {
+        public int[] _data = new int[25]; //배열
+        //public MyList<int> _data2 = new MyList<int>(); //동적 배열
+        public MyLinkedList<int> _data3 = new MyLinkedList<int>(); //연결 리스트
+
+        public void Initialize()
+        {
+            //동적 배열
+            /*
+            _data2.Add(101);
+            _data2.Add(102);
+            _data2.Add(103);
+            _data2.Add(104);
+            _data2.Add(105);
+
+            int temp = _data2[2];
+
+            _data2.RemoveAt(2);
+            */
+
+            //연결 리스트
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            MyLinkedListNode<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
+
+            _data3.Remove(node);
+        }
+    }
+}
